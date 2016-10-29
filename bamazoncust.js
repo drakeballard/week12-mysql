62b1e4b90de58009be0bd624574c3fc4fefe6f9b
Drake Ballard <drakeballard@gmail.com>
2016-10-29 18:52:15 -0500

    adding some pseudocode to the js file for it to fully operate

--- bamazoncust.js
+++ bamazoncust.js
@@ -10,3 +10,28 @@ var connection = mysql.createConnection({
     password:"",
     database: "bamazon"
 })
+
+//Need to Connect to the Bamazon database
+
+
+//Need to create a Bamazon Catalog that is available for sale
+
+
+
+//need to create some prompting for the customer to initiate what they want to purchase, and by how muuch. This could be initially a 2 part function
+
+
+
+//However, the inventory needs to be available
+
+
+
+//After the sale occured the store needs to update the remaining quanities
+
+
+
+//provide a itemize breakdown on what the customer is going to purchase
+
+
+
+//determine if the customer wants to purchase more items (prompt   yay or nay)
